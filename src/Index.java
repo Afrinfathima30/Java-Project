8355fb13627db45504f3189c5d64bcb4b747ccc5
Afrin Fathima <afrinbasheer30@gmail.com>
2025-10-28 15:11:34 +0530

    Simple Java program to Calculate the Simple Interest

--- src/Index.java
+++ src/Index.java
@@ -1,5 +1,17 @@
-public class Index {
-    public static void main(String[] args) {
-        System.out.println("Hello Afrin!");
+import java.util.*;
+public class Index{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Principle");
+        int p= sc.nextInt();
+        System.out.println("Enter the Rate of Interest");
+        float r=sc.nextFloat();
+//        System.out.println("Enter the year");
+//        int y= sc.nextInt();
+        for(int y=1978;y<=2005;y++){
+            double sp = (p*r*y)/100;
+            System.out.println("The Simple interest of the "+y+" is "+sp);
+        }
+
     }
 }
