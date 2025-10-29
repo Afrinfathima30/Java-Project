4a1b5a383b86cd3ac5b6f984ba5cc17e05fffe30
Afrin Fathima <afrinbasheer30@gmail.com>
2025-10-29 14:29:13 +0530

    simple java program to Accept String as command line arguments

---
+++ src/FunctionsDemo.java
@@ -0,0 +1,8 @@
+public class FunctionsDemo {
+    public static void main(String[] args){
+
+    }
+    public static void add(){
+
+    }
+}
---
+++ src/StringDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringDemo {
+    public static void main(String[] args){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter the first Word");
+     String w1= sc.next();
+     System.out.println("Enter the Second Word");
+     String w2=sc.next();
+     System.out.println("The Word is: "+w1+" Technologies "+w2);
+    }
+}
